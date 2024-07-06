764c13c5c933fbcde5e4139d8ed0755243540101
banna117 <banna111222@gmail.com>
2024-07-06 17:48:50 +0900

    feat: 무ㅓ라도 뜸

--- lib/components/group_tap.dart
+++ lib/components/group_tap.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class GroupTap extends StatelessWidget {
   final int groupId;
@@ -7,12 +7,19 @@ class GroupTap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/group/', arguments: groupId);
+      },
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.symmetric(vertical: 8.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.blue),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Text('Group $groupId'),
       ),
-      child: Text('Group $groupId'),
     );
   }
 }
--- lib/components/login.dart
+++ lib/components/login.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Login Component');
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Username'),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // 로그인 버튼을 눌렀을 때 메인 페이지로 이동
+              Navigator.pushReplacementNamed(context, '/main');
+            },
+            child: Text('Login'),
+          ),
+        ],
+      ),
+    );
   }
 }
