d4ae6da1edc9bc7f06cc800f661a38c6448e37fd
banna117 <banna111222@gmail.com>
2024-07-07 16:05:58 +0900

    feat: pollpage design

--- lib/pages/poll_page.dart
+++ lib/pages/poll_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nymmp_front/components/custom_button.dart';
 
 class PollPage extends StatelessWidget {
   final int pollId;
@@ -8,9 +9,178 @@ class PollPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('투표 $pollId')),
+      // appBar: AppBar(title: Text('투표 $pollId')),
       body: Center(
-        child: Text('투표 $pollId', style: TextStyle(fontSize: 24)),
+        child: Container(
+          constraints: BoxConstraints(maxWidth: 414), // 최대 너비를 414로 설정
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: Offset(-4, 0), // 왼쪽 그림자
+                blurRadius: 4,
+                spreadRadius: 2,
+              ),
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: Offset(4, 0), // 오른쪽 그림자
+                blurRadius: 4,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 20.0), // 좌우 패딩 추가
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 100), // 상단 여백 추가
+              // 첫 번째 요소: 랜덤 이미지
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.grey,
+                margin: EdgeInsets.only(top: 20.0), // 상단 마진 추가
+                child: Center(
+                  child: Text(
+                    'Random Image',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // 두 번째 요소: 질문 텍스트
+              Text(
+                questionText,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 50),
+              // 세 번째 요소: 2x2 버튼 배열
+              Container(
+                width: double.infinity,
+                height: 180,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0), // 큰 사각형 밖에 패딩 추가
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          width: 116,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 1",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Container(
+                          width: 116,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 2",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        child: Container(
+                          width: 116,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 3",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          width: 116,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 4",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // 네 번째 요소: Skip 및 Shuffle 버튼
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CustomButton(
+                    onPressed: () {},
+                    text: "Shuffle",
+                  ),
+                  SizedBox(height: 20,),
+                  CustomButton(
+                    onPressed: () {},
+                    text: "Skip",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
