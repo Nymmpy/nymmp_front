c0a61f02f698a16f38b38a703f16d5576bcbb791
banna117 <banna111222@gmail.com>
2024-07-08 12:29:16 +0900

    feat: poll page option design

--- lib/pages/poll_page.dart
+++ lib/pages/poll_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nymmp_front/components/custom_button.dart';
 
-class PollPage extends StatelessWidget {
+class PollPage extends StatefulWidget {
   final int pollId;
   final String questionText;
 
@@ -10,9 +11,21 @@ class PollPage extends StatelessWidget {
   });
 
   @override
+  _PollPageState createState() => _PollPageState();
+}
+
+class _PollPageState extends State<PollPage> {
+  String? selectedOption;
+
+  void _handleOptionSelect(String option) {
+    setState(() {
+      selectedOption = option;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('투표 $pollId')),
       body: Center(
         child: Container(
           constraints: BoxConstraints(maxWidth: 414), // 최대 너비를 414로 설정
@@ -33,15 +46,17 @@ class PollPage extends StatelessWidget {
               ),
             ],
           ),
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.symmetric(horizontal: 20.0), // 좌우 패딩 추가
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              SizedBox(height: 100), // 상단 여백 추가
               // 첫 번째 요소: 랜덤 이미지
               Container(
-                width: double.infinity,
-                height: 200,
+                width: 100,
+                height: 100,
                 color: Colors.grey,
+                margin: EdgeInsets.only(top: 20.0), // 상단 마진 추가
                 child: Center(
                   child: Text(
                     'Random Image',
@@ -52,121 +67,166 @@ class PollPage extends StatelessWidget {
               SizedBox(height: 20),
               // 두 번째 요소: 질문 텍스트
               Text(
-                questionText,
+                widget.questionText,
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.w400,
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 50),
               // 세 번째 요소: 2x2 버튼 배열
               Container(
-                padding: EdgeInsets.all(20.0), // 큰 사각형 밖에 패딩 추가
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Container(
-                        width: 116,
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Option 1",
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
+                width: double.infinity,
+                height: 200, // height를 약간 키움
+                child: Padding(
+                  padding: EdgeInsets.all(20.0), // 큰 사각형 밖에 패딩 추가
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: ElevatedButton(
+                          onPressed: () => _handleOptionSelect('Option 1'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(
+                              color: selectedOption == 'Option 1'
+                                  ? Colors.grey
+                                  : Colors.black,
+                              width: 4, // 테두리를 두껍게 설정
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            minimumSize: Size(130, 70), // 버튼 크기를 키움
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 1",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: selectedOption == 'Option 1'
+                                    ? Colors.grey
+                                    : Colors.black,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 116,
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Option 2",
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: ElevatedButton(
+                          onPressed: () => _handleOptionSelect('Option 2'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(
+                              color: selectedOption == 'Option 2'
+                                  ? Colors.grey
+                                  : Colors.black,
+                              width: 4, // 테두리를 두껍게 설정
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            minimumSize: Size(130, 70), // 버튼 크기를 키움
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 2",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: selectedOption == 'Option 2'
+                                    ? Colors.grey
+                                    : Colors.black,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Container(
-                        width: 116,
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Option 3",
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        child: ElevatedButton(
+                          onPressed: () => _handleOptionSelect('Option 3'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(
+                              color: selectedOption == 'Option 3'
+                                  ? Colors.grey
+                                  : Colors.black,
+                              width: 4, // 테두리를 두껍게 설정
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            minimumSize: Size(130, 70), // 버튼 크기를 키움
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 3",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: selectedOption == 'Option 3'
+                                    ? Colors.grey
+                                    : Colors.black,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                        width: 116,
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Option 4",
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: ElevatedButton(
+                          onPressed: () => _handleOptionSelect('Option 4'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(
+                              color: selectedOption == 'Option 4'
+                                  ? Colors.grey
+                                  : Colors.black,
+                              width: 4, // 테두리를 두껍게 설정
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            minimumSize: Size(130, 70), // 버튼 크기를 키움
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Option 4",
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w400,
+                                color: selectedOption == 'Option 4'
+                                    ? Colors.grey
+                                    : Colors.black,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               SizedBox(height: 20),
               // 네 번째 요소: Skip 및 Shuffle 버튼
-              Row(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  ElevatedButton(
+                  CustomButton(
                     onPressed: () {},
-                    child: Text("Skip"),
+                    text: "Shuffle",
                   ),
-                  ElevatedButton(
+                  SizedBox(height: 20),
+                  CustomButton(
                     onPressed: () {},
-                    child: Text("Shuffle"),
+                    text: "Skip",
                   ),
                 ],
               ),
